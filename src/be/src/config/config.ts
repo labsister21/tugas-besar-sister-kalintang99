14c3ae39f3673fa2fc24aec78aaef5c4b52f8db9
LastPrism7 <maulanamuhammadsusetyo@gmail.com>
2025-06-05 23:45:02 +0700

    refactor: timeout

--- src/be/src/config/config.ts
+++ src/be/src/config/config.ts
@@ -2,8 +2,8 @@ export const RaftConfig = {
   heartBeat: {
     checkInterval: 50,
     sendInterval: 50,
-    min_timeout: 6000,
-    max_timeout: 9000,
+    min_timeout: 1000,
+    max_timeout: 2000,
     sendTimeout: 300,
   },
   logCompaction: {
